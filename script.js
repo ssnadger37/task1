f487222d64651d9bace5c16550caad8ea25ca1b8
Samprit S Nadger <71712514+ssnadger37@users.noreply.github.com>
2023-11-15 20:56:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("submitButton").onclick = function(){
-    var email = document.getElementById("emailInput").value;
-    if(email !== '') {
-        window.location.href = "page2.html"
-    } else{
-        alert("Please Enter an email !!")
-    }
-};
